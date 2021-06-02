44e17b0dc732ad46cc40f46f4b5ea6114eb1fe74
April Joy Miller <49565059+apriljmiller4592@users.noreply.github.com>
2021-06-01 17:19:40 -0700

    Add files via upload

---
+++ PythonMusic.rb
@@ -0,0 +1,56 @@
+# Welcome to Sonic Pi
+#Code by April
+
+use_synth :fm
+live_loop :flute do
+  sample :ambi_choir, rate: 0.3
+  sample :bd_haus, rate: 1
+  sleep 0.65
+  play :C
+  sleep 0.4
+  play :E
+  sleep 0.4
+  play :G
+  sleep 0.4
+  play :E
+  play :C
+  sleep 0.4
+  play :E
+  sleep 0.4
+  play :G
+  sleep 0.4
+  play :E
+  sleep 0.4
+  play :A
+  sleep 0.4
+  play :C
+  sleep 0.4
+  play :E
+  sleep 0.4
+  play :C
+  sleep 0.4
+  play :A
+  sleep 0.4
+  play :C
+  sleep 0.4
+  play :E
+  sleep 0.4
+  play :C
+  sleep 0.4
+  
+  play :F
+  sleep 0.4
+  play :A
+  sleep 0.4
+  play :C
+  sleep 0.4
+  play :A
+  play :F
+  sleep 0.4
+  play :A
+  sleep 0.4
+  play :C
+  sleep 0.4
+  play :A
+  
+end
